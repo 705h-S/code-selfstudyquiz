7d25ac4f872adfbd598e16775535f1020237b130
705H-S <93358442+705h-S@users.noreply.github.com>
2021-12-30 01:20:19 -0600

    pseudocode steps out for js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+// create an addeventlistener so that when button is clicked timer counts down and HTML content changes 
+// addeventlistner for when highscores is click itll show the the high scores that are saved locally 
+// have a set time function count down from 1min, if time hits 0 html changes to a game over screen
+// create booleang variables of correct answers 
+// create undefined variables and have user define them with initals for highscore
+// if user clicks true answer display "correct" and move to next question
+// else user clicks false answer display "wrong" move to next question and subtract -10 secounds from timer.
+// console log and console localy save remaining secounds after user completes quiz and have that be highscore 
+// more secounds left on the timer = higher score 
+//  create function to have higher score display first 
+// 
+
+
 console.log ("hello world");
