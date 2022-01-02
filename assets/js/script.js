00d3e8888fa40961f963102e5405873d9a5dde2c
705H-S <93358442+705h-S@users.noreply.github.com>
2022-01-02 13:53:50 -0800

    created array with questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // create an addeventlistener so that when button is clicked timer counts down and HTML content changes 
 // addeventlistner for when highscores is click itll show the the high scores that are saved locally 
 // have a set time function count down from 1min, if time hits 0 html changes to a game over screen
-// create booleang variables of correct answers 
+// create variables of question array  with objects of questions and answers
 // create undefined variables and have user define them with initals for highscore
 // if user clicks true answer display "correct" and move to next question
 // else user clicks false answer display "wrong" move to next question and subtract -10 secounds from timer.
@@ -13,6 +13,43 @@
 
 var startBtn = document.querySelector(".button");
 var timer = document.getElementById("time");
+
+ // An array with the questions and answers that will show up in the quiz
+const questions = [
+    {
+        ask: " This is the first generic question? \n (a) first answer\n (b) secound answer\n (c) Third answer\n (d) fourth answer",
+        answer: "b"
+    },
+
+    {
+        ask: " This is the secound generic question? \n (a) first answer\n (b) secound answer\n (c) Third answer\n (d) fourth answer",
+        answer: "d"
+    },
+
+    {
+        ask: " This is the third generic question? \n (a) first answer\n (b) secound answer\n (c) Third answer\n (d) fourth answer",
+        answer: "a"
+    },
+
+    {
+        ask: " This is the fourth generic question? \n (a) first answer\n (b) secound answer\n (c) Third answer\n (d) fourth answer",
+        answer: "c"
+    },
+
+    {
+        ask: " This is the fith  generic question? \n (a) first answer\n (b) secound answer\n (c) Third answer\n (d) fourth answer",
+        answer: "a"
+    },
+
+    {
+        ask: " This is the sixth  generic question? \n (a) first answer\n (b) secound answer\n (c) Third answer\n (d) fourth answer",
+        answer: "d"
+    }
+    
+
+]
+// score is initially set to 0 with time left define in score
+let score = 0;
 
 
 
